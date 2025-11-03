182d89e64e4893a0f641c9a7dcb7ee50615f4638
meba1627 <meba1627@gmail.com>
2025-11-03 12:07:21 +0300

    Add files via upload

---
+++ example (4).py
@@ -0,0 +1,11 @@
+classify_person=int(input("Enter your age:"))
+if 0<classify_person<13:
+        print("Child")
+elif 12<classify_person<18:
+    print("Teenager")
+elif 17<classify_person<65:
+	print("Teenager")
+elif 18<= age <= 64:
+	print("Adult")
+else:
+	print("Senior")
